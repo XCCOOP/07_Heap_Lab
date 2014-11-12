3bbea9d459da0836db31f8570b2be4ceee95a7cb
XCCOOP <country043@frontier.com>
2014-11-11 23:04:33 -0500

    Finished heap lab

--- Heap.h
+++ Heap.h
@@ -1,3 +1,10 @@
+/*
+* Author: Nick Cooperrider
+* Date: 11/11/2014
+* All code was written by myself with examples taken from the reading and in class.
+*
+*/
+
 #include "Queue.h"
 #include <utility>
 
@@ -59,36 +66,84 @@ Heap<Pri,T>::Heap(){
 
 template<class Pri, class T>
 Heap<Pri,T>::~Heap(){
-  //TODO
+	delete[] backingArray;
+	numItems = 0;
+	arrSize = 0;
 }
 
 template<class Pri, class T>
 void Heap<Pri,T>::grow(){
-  //TODO
+	
+	arrSize = arrSize * 2;
+	std::pair<Pri, T>* tempArray = new std::pair<Pri, T>[arrSize];
+
+	for(int i = 0; i < numItems; i++) {
+		tempArray[i] = backingArray[i];
+	}
+
+	backingArray = tempArray;
+	delete tempArray;
 }
 
 template<class Pri, class T>
 void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
-	if(numItems == 0) {
-		backingArray[0]->Pri = toAdd->Pri;
-		backingArray[0]->T = toAdd->T;
+	
+	if(numItems == arrSize) {
+		grow();
 	}
+
+	backingArray[numItems] = toAdd;
+	bubbleUp(numItems);
+	numItems++;
 }
 
 template<class Pri, class T>
 void Heap<Pri,T>::bubbleUp(unsigned long index){
-  //TODO
+
+	int parent = (index - 1)/2;
+
+	if (backingArray[parent] > backingArray[index] && index > 0) {
+		backingArray[parent].swap(backingArray[index]);
+		index = parent;
+		bubbleUp(parent);
+	}
 }
 
 template<class Pri, class T>
 void Heap<Pri,T>::trickleDown(unsigned long index){
-  //TODO
-}
+
+	do {
+ 		int count = -1;
+ 		int left = (2 * index) + 1;
+ 		int right = (2 * index) + 2;
+ 
+ 		if (backingArray[index] > backingArray[left] || backingArray[index] > backingArray[right]) {
+ 			if (backingArray[left] < backingArray[right])
+ 				count = left;
+ 			else
+				count = right;
+ 		}
+ 
+ 		if (count >= 0)
+ 			backingArray[index].swap(backingArray[count]);
+ 
+ 
+ 		index = count;
+ 	} while (index <= 0);
+ }
 
 template<class Pri, class T>
 std::pair<Pri,T> Heap<Pri,T>::remove(){
-  //TODO
-  std::pair<Pri,T> tmp;
+
+	if(numItems == 0) {
+		throw std::string("Error, no items to remove");
+	}
+
+	std::pair<Pri,T> tmp = backingArray[0];
+	backingArray[0] = backingArray[(numItems - 1)];
+	numItems--;
+	trickleDown(0);
+
   return tmp;
 }
 
